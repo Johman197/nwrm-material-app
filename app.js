7fd1dbd597fae8cfd2a19fd75a060d9973bcb5a6
Caleb Edwards <Calebedwards197@gmail.com>
2022-10-04 16:05:20 -0700

    JavaScript Testing
    Unable to make CSS changes with JavaScript

--- app.js
+++ app.js
@@ -1,22 +1,46 @@
+const form = document.getElementsByClassName('sign-up-form')
 const firstName = document.getElementById('fName');
 const lastName = document.getElementById('lName');
 const email = document.getElementById('fName');
 const password = document.getElementById('fName');
-const form = document.getElementsByTagName('div');
+const myForm = document.getElementById('my-div');
 
-document.addEventListener('submit', e =>{
-    e.preventDefault();
-
-    checkInputs();
+form.addEventListener('submit', e => {
+	e.preventDefault();
+	
+	checkInputs();
 });
 
-function checkInputs(){
-    const firstNameValue = firstName.value.trim();
-    const lastName = lastName.value.trim();
-    const email = email.value.trim();
-    const password = password.value.trim();
 
-    if(firstNameValue === ''){
-        firstName.parentElement.classList.remove("border-accent");
-    }
+function checkInputs() {
+	// trim to remove the whitespaces
+	const firstNameValue = username.value.trim();
+	const lastNameValue = email.value.trim();
+	const passwordValue = password.value.trim();
+	
+	if(firstNameValue === '') {
+		setErrorFor(firstName);
+	}
 }
+
+    function setErrorFor(input) {
+        const formControl = input.parentElement;
+        formControl.className = 'border-accent border-accent-red';
+    }
+
+    // function checkInputs(){
+//     const firstNameValue = firstName.value.trim();
+//     const lastNameValue = lastName.value.trim();
+//     const emailValue = email.value.trim();
+//     const passwordValue = password.value.trim();
+
+//     if(lastNameValue === ''){
+//         setErrorFor(firstName);
+//     }
+// }
+
+// function setErrorFor(input){
+//     const formControl = input.parentElement;
+
+//     formControl.className = 'border-accent border-accent-red';
+//}
